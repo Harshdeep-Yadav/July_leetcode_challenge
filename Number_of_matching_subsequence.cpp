6da963f1644db762ac75a321ab9139662b4cc173
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-07-20 23:30:08 +0530

    initial commit

---
+++ Number_of_matching_subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int n=words.size();
+        map<char,vector<int>>m;
+        int i=0;
+        for(auto x:s){
+            m[x].push_back(i++);    
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            string curr=words[i];
+            int latest=-1;
+            for(int j=0;j<curr.size();j++)
+            {
+                auto it=upper_bound(m[curr[j]].begin(),m[curr[j]].end(),latest);
+                    if(it==m[curr[j]].end()) break;
+                latest=*it;
+                if(j==curr.size()-1)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
