67eee78f7f81d39520f07231b418bcb6366bd08d
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-07-27 17:41:27 +0530

    initial commit

---
+++ flattened_a_binary_tree_into_linked_list.cpp
@@ -0,0 +1,16 @@
+
+
+class Solution {
+ TreeNode * prev = NULL;
+  public:
+    void flatten(TreeNode * root) {
+      if (root == NULL) return;
+
+      flatten(root -> right);
+      flatten(root -> left);
+
+      root -> right = prev;
+      root -> left = NULL;
+      prev = root;
+    }
+};
